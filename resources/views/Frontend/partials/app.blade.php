db2742f3283159f46875b8161cc1e19934fca3b9
Saranya <bostondynamicsdeepdream@gmail.com>
2023-09-08 17:05:32 +0530

    logo changes

--- resources/views/Frontend/partials/app.blade.php
+++ resources/views/Frontend/partials/app.blade.php
@@ -29,7 +29,7 @@
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body class="goto-here">
-		<div class="py-1 bg-primary">
+		<div class="py-1 bg-pink">
     	<div class="container">
     		<div class="row no-gutters d-flex align-items-start align-items-center px-md-0">
 	    		<div class="col-lg-12 d-block">
