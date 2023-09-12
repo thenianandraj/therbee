b63bd7edb4e54f6dec89c29bfc5fd14efca05c89
Saranya <bostondynamicsdeepdream@gmail.com>
2023-09-12 11:31:08 +0530

    filter changes

--- resources/views/Frontend/partials/header.blade.php
+++ resources/views/Frontend/partials/header.blade.php
@@ -11,24 +11,14 @@
             <span class="input-group-text custom-pink-bg"><i class="icon-search"></i></span>
         </div>
     </div>
-    
       <div class="collapse navbar-collapse" id="ftco-nav">
           <ul class="navbar-nav ml-auto">
-              <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
-              <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Shop</a>
-                  <div class="dropdown-menu" aria-labelledby="dropdown04">
-                      <a class="dropdown-item" href="shop.html">Shop</a>
-                      <a class="dropdown-item" href="wishlist.html">Wishlist</a>
-                      <a class="dropdown-item" href="product-single.html">Single Product</a>
-                      <a class="dropdown-item" href="cart.html">Cart</a>
-                      <a class="dropdown-item" href="checkout.html">Checkout</a>
-                  </div>
-              </li>
-              <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
-              <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
-              <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
-              <li class="nav-item cta cta-colored"><a href="cart.html" class="nav-link"><span class="icon-shopping_cart"></span>[0]</a></li>
+              <li class="nav-item active"><a href="index.html" class="nav-link">HOME</a></li>
+              <li class="nav-item"><a href="about.html" class="nav-link">ABOUT</a></li>
+              <li class="nav-item"><a href="blog.html" class="nav-link">MYORDERS</a></li>
+              <li class="nav-item"><a href="blog.html" class="nav-link">wISHLIST</a></li>
+              <li class="nav-item"><a href="contact.html" class="nav-link">CONTACT</a></li>
+              <li class="nav-item cta cta-colored"><a href="cart.html" class="nav-link"><span class="icon-shopping_cart"></span>LOGIN[0]</a></li>
           </ul>
       </div>
   </div>
